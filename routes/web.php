9a48c82db052e3d6c617d5cdf5a2236b29051f1d
reza.irwanto <reza.irwanto@stockbit.com>
2021-09-03 20:48:22 +0700

    set default url

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index']);
 
 Route::get('/register-school', function() {
     return view('register-school');
